e8721552e9e22139b01a7e6c6bc3f7eb09f9316c
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2025-05-06 22:41:04 +0900

    Front/Feat: enhance Header component with wallet dropdown functionality
    - Integrated WalletDropdown and WalletDropdownDisconnect for improved wallet management.
    - Added Identity, Address, and EthBalance components to display user information in the header.
    - Updated imports from OnchainKit to support new features.

--- front/src/app/components/Header.tsx
+++ front/src/app/components/Header.tsx
@@ -1,8 +1,10 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Wallet, ConnectWallet } from '@coinbase/onchainkit/wallet';
-import { Avatar, Name } from '@coinbase/onchainkit/identity';
+import { Wallet, ConnectWallet, WalletDropdown, WalletDropdownDisconnect } from '@coinbase/onchainkit/wallet';
+import { Avatar, Name, Address, EthBalance, Identity } from '@coinbase/onchainkit/identity';
+import { color } from '@coinbase/onchainkit/theme';
+
 export function Header() {
   const pathname = usePathname();
   return (
@@ -40,6 +42,15 @@ export function Header() {
                 <Avatar className="h-6 w-6" />
                 <Name />
               </ConnectWallet>
+              <WalletDropdown>
+                <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+                  <Avatar />
+                  <Name />
+                  <Address className={color.foregroundMuted} />
+                  <EthBalance />
+                </Identity>
+                <WalletDropdownDisconnect />
+              </WalletDropdown>
             </Wallet>
           </div>
         </div>
